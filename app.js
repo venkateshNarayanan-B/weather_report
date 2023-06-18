453bab49978fc0e03a7c3b08a6bc9362c7dacf2e
venki <getvenkatesh.b@gmail.com>
2023-06-18 11:23:44 +0530

    created server in port 3000

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+//initiate required lib to the project
 const express       =   require('express');
 const bodyParser    =   require('body-parser');
+
+//extend express to app
+const app           =   express();
+
+
+//create port and start the server
+app.listen(3000, function(){
+    console.log("Server is up in port 3000");
+});
+
